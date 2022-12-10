428de03e26b54be80a7665daff1bcb9cd6dc4058
mateusjrcavalcanti <111059128+mateusjrcavalcanti@users.noreply.github.com>
2022-12-10 17:14:42 +0000

    Produtos

--- Projeto.sql
+++ Projeto.sql
@@ -3,28 +3,28 @@ DROP SCHEMA IF EXISTS public CASCADE;
 CREATE SCHEMA IF NOT EXISTS public;
 
 ------> CRIANDO TABELAS E COLUNAS
-CREATE TABLE IF NOT EXISTS "Categoria"
+CREATE TABLE "Categoria"
 (
     codcategoria SERIAL,
     categoria character varying(255),
     CONSTRAINT "Categoria_pkey" PRIMARY KEY (codcategoria)
 );
 
-CREATE TABLE IF NOT EXISTS "Cidade"
+CREATE TABLE "Cidade"
 (
     codcidade SERIAL,
     cidade character varying,
     CONSTRAINT "Cidade_pkey" PRIMARY KEY (codcidade)
 );
 
-CREATE TABLE IF NOT EXISTS "Departamento"
+CREATE TABLE "Departamento"
 (
     coddepartamento SERIAL,
     departamento character varying,
     CONSTRAINT "Departamento_pkey" PRIMARY KEY (coddepartamento)
 );
 
-CREATE TABLE IF NOT EXISTS "Loja"
+CREATE TABLE "Loja"
 (
     codloja SERIAL,
     nome character varying,
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS "Loja"
     REFERENCES "Cidade" (codcidade)
 );
 
-CREATE TABLE IF NOT EXISTS "Fornecedor"
+CREATE TABLE "Fornecedor"
 (
     codfornecedor SERIAL,
     fornecedor character varying,
@@ -49,7 +49,6 @@ CREATE TABLE IF NOT EXISTS "Fornecedor"
     num integer,
     bairro character varying,
     cep character(9),
-    contato "char",
     cnpj character(18),
     insc character varying,
     tel character(14),
@@ -59,7 +58,7 @@ CREATE TABLE IF NOT EXISTS "Fornecedor"
     REFERENCES "Cidade" (codcidade)
 );
 
-CREATE TABLE IF NOT EXISTS "Transportadora"
+CREATE TABLE "Transportadora"
 (
     codtransportadora SERIAL,
     transportadora character varying,
@@ -70,14 +69,13 @@ CREATE TABLE IF NOT EXISTS "Transportadora"
     cnpj character(16),
     tel character(14),
     insc character varying,
-    contato character varying,
     "codcidade" integer,
     CONSTRAINT "Transportadora_pkey" PRIMARY KEY (codtransportadora),
 	CONSTRAINT "Transportadora_codcidade_fkey" FOREIGN KEY ("codcidade")
     REFERENCES "Cidade" (codcidade)
 );
 
-CREATE TABLE IF NOT EXISTS "Produto"
+CREATE TABLE "Produto"
 (
     codproduto SERIAL,
     descricao character varying,
@@ -92,7 +90,7 @@ CREATE TABLE IF NOT EXISTS "Produto"
     REFERENCES "Categoria" (codcategoria)
 );
 
-CREATE TABLE IF NOT EXISTS "Entrada"
+CREATE TABLE "Entrada"
 (
     codentrada SERIAL,
     dataped date,
@@ -110,7 +108,7 @@ CREATE TABLE IF NOT EXISTS "Entrada"
     REFERENCES "Loja" (codloja)
 );
 
-CREATE TABLE IF NOT EXISTS "ItemEntrada"
+CREATE TABLE "ItemEntrada"
 (
     coditementrada SERIAL,
     lote character varying,
@@ -125,7 +123,7 @@ CREATE TABLE IF NOT EXISTS "ItemEntrada"
     REFERENCES "Produto" (codproduto)
 );
 
-CREATE TABLE IF NOT EXISTS "Saida"
+CREATE TABLE "Saida"
 (
     codsaida SERIAL,
     total double precision,
@@ -140,7 +138,7 @@ CREATE TABLE IF NOT EXISTS "Saida"
     REFERENCES "Loja" (codloja)
 );
 
-CREATE TABLE IF NOT EXISTS "ItemSaida"
+CREATE TABLE "ItemSaida"
 (
     coditemsaida SERIAL,
     lote character varying,
@@ -155,7 +153,7 @@ CREATE TABLE IF NOT EXISTS "ItemSaida"
     REFERENCES "Produto" (codproduto)
 );
 
-CREATE TABLE IF NOT EXISTS "Funcionario"
+CREATE TABLE "Funcionario"
 (
     codfuncionario SERIAL,
     funcionario character varying,
@@ -166,7 +164,7 @@ CREATE TABLE IF NOT EXISTS "Funcionario"
     REFERENCES "Loja" (codloja)
 );
 
-CREATE TABLE IF NOT EXISTS "Funcionario_Departamento"
+CREATE TABLE "Funcionario_Departamento"
 (
     codfuncionario serial,
     coddepartamento serial,
@@ -177,6 +175,12 @@ CREATE TABLE IF NOT EXISTS "Funcionario_Departamento"
     FOREIGN KEY (coddepartamento)
     REFERENCES "Departamento" (coddepartamento) ON DELETE CASCADE
 );
+
+----> EDITANDO TABELAS
+
+--* Coluna de Contato *--
+ALTER TABLE "Transportadora" ADD COLUMN IF NOT EXISTS contato character varying;
+ALTER TABLE "Fornecedor" ADD COLUMN IF NOT EXISTS contato character varying;
 
 ----> INSERINDO DADOS
 
@@ -220,10 +224,10 @@ INSERT INTO "Loja" VALUES (DEFAULT, 'Rezende', 'Rua Paulo Plinio', 310, 'cohab',
 INSERT INTO "Loja" VALUES (DEFAULT, 'Casas Amazonas', 'Rua Godoy', 418, 'areia branca', '56302300', '1234567891234', '987654321', '876.87678678687', 1);
 
 --* Transportadoras *--
-INSERT INTO "Transportadora" VALUES (DEFAULT, 'Azul', 'Rua Rio Negro', 206, 'centro',  '56302300', '1234567891234', '987654321', '78678687687676', 'gasparzinho', 2);
-INSERT INTO "Transportadora" VALUES (DEFAULT, 'Amarelo', 'Rua Rio Negro', 386, 'henrique café',  '56302300', '1234567891234', '987654321', '000111000222333555', 'luizinho' , 2);
-INSERT INTO "Transportadora" VALUES (DEFAULT, 'Preto', 'Rua Rio Negro', 96, 'castelo branco',  '56302300', '6546546546', '654654656546', '64565654354345', 'pedrindo', 1);
-INSERT INTO "Transportadora" VALUES (DEFAULT, 'Branco', 'Rua Rio Negro', 206, 'vila eduarda',  '56302300', '54654646', '654654654', '5876783873783', 'joaozinho', 1);
+INSERT INTO "Transportadora" VALUES (DEFAULT, 'Azul', 'Rua Rio Negro', 206, 'centro',  '56302300', '1234567891234', '987654321', '78678687687676', 2, 'gasparzinho');
+INSERT INTO "Transportadora" VALUES (DEFAULT, 'Amarelo', 'Rua Rio Negro', 386, 'henrique café',  '56302300', '1234567891234', '987654321', '000111000222333555', 2, 'luizinho');
+INSERT INTO "Transportadora" VALUES (DEFAULT, 'Preto', 'Rua Rio Negro', 96, 'castelo branco',  '56302300', '6546546546', '654654656546', '64565654354345', 1, 'pedrindo');
+INSERT INTO "Transportadora" VALUES (DEFAULT, 'Branco', 'Rua Rio Negro', 206, 'vila eduarda',  '56302300', '54654646', '654654654', '5876783873783', 1, 'joaozinho');
  
 --* Funcionarios *--
 INSERT INTO "Funcionario"  VALUES (DEFAULT, 'Fulano de Paula', 1);
@@ -250,14 +254,51 @@ INSERT INTO "Funcionario_Departamento"  VALUES (5, 1, '2016-10-11');
 INSERT INTO "Funcionario_Departamento"  VALUES (4, 3, '2016-11-12');
 
 --* Fornecedores *--
-INSERT INTO "Fornecedor" VALUES (DEFAULT, 'Brasilian Imports', 'Rua Dona Celma', 26, 'centro', '56509300', 'Sr. João', '6564656655', '65465465465465', '+5581855526312', 1);
-INSERT INTO "Fornecedor" VALUES (DEFAULT, 'Distribuidora de Acessórios', 'Rua Barão do Mar Azul', 64, 'cohab', '56509300', 'Huan Iang', '6564656655', '65465465465465', '+5581855526312', 2);
-INSERT INTO "Fornecedor" VALUES (DEFAULT, 'Atacadão', 'Rua Antonio barboza', 124, 'areia amarela', '56429300', 'Sr. e Sra. Smith', '6564656655', '65465465465465', '+5581855526312', 1);
-INSERT INTO "Fornecedor" VALUES (DEFAULT, 'Leve Tudo', 'Rua 20', 224, 'henrique café', '56509450', 'Baba Yaga', '6564656655', '65465465465465', '+5581855526312', 2);
-INSERT INTO "Fornecedor" VALUES (DEFAULT, 'China Importados', 'Rua 18', 234, 'idalino souza', '56329300', 'Ana Florinda', '6564656655', '65465465465465', '+5581855526312', 1);
-INSERT INTO "Fornecedor" VALUES (DEFAULT, 'IDistribuidora', 'Rua pão de açucar', 241, 'centro', '56339300', 'Isa newton', '6564656655', '65465465465465', '+5581855526312', 2);
-INSERT INTO "Fornecedor" VALUES (DEFAULT, 'John LTDA', 'Av. das nações desunidas', 248, 'ana auxiliadora', '56509110', 'john wick', '6564656655', '65465465465465', '+5581855526312', 1);
-INSERT INTO "Fornecedor" VALUES (DEFAULT, 'BH Imports', 'Rua Dona Celma', 240, 'centro', '56509990', 'johnny', '6564656655', '65465465465465', '+5581855526312', 2);
+INSERT INTO "Fornecedor" VALUES (DEFAULT, 'Brasilian Imports', 'Rua Dona Celma', 26, 'centro', '56509300', '6564656655', '65465465465465', '+5581855526312', 1, 'Sr. João');
+INSERT INTO "Fornecedor" VALUES (DEFAULT, 'Distribuidora de Acessórios', 'Rua Barão do Mar Azul', 64, 'cohab', '56509300', '6564656655', '65465465465465', '+5581855526312', 2, 'Huan Iang');
+INSERT INTO "Fornecedor" VALUES (DEFAULT, 'Atacadão', 'Rua Antonio barboza', 124, 'areia amarela', '56429300', '6564656655', '65465465465465', '+5581855526312', 1, 'Sr. e Sra. Smith');
+INSERT INTO "Fornecedor" VALUES (DEFAULT, 'Leve Tudo', 'Rua 20', 224, 'henrique café', '56509450', '6564656655', '65465465465465', '+5581855526312', 2, 'Baba Yaga');
+INSERT INTO "Fornecedor" VALUES (DEFAULT, 'China Importados', 'Rua 18', 234, 'idalino souza', '56329300', '6564656655', '65465465465465', '+5581855526312', 1, 'Ana Florinda');
+INSERT INTO "Fornecedor" VALUES (DEFAULT, 'IDistribuidora', 'Rua pão de açucar', 241, 'centro', '56339300', '6564656655', '65465465465465', '+5581855526312', 2, 'Isa newton');
+INSERT INTO "Fornecedor" VALUES (DEFAULT, 'John LTDA', 'Av. das nações desunidas', 248, 'ana auxiliadora', '56509110', '6564656655', '65465465465465', '+5581855526312', 1, 'john wick');
+INSERT INTO "Fornecedor" VALUES (DEFAULT, 'BH Imports', 'Rua Dona Celma', 240, 'centro', '56509990', '6564656655', '65465465465465', '+5581855526312', 2, 'johnny');
+
+--* Produtos [ codproduto | descricao | peso | qtdemin | codcategoria | codfornecedor] *--
+
+/*
+	CATEGORIAS SEM PRODUTOS:
+		Joelheira
+		Joias
+		Lenço
+		Luva
+		Malas
+		Máscara de plástico
+		Óculos (lentes de vidro)
+		Óculos de sol
+		Relógio
+*/
+
+INSERT INTO "Produto"  VALUES (DEFAULT, 'Bolsa com detalhe de costura', 1.256, 1, 2, 1);
+INSERT INTO "Produto"  VALUES (DEFAULT, 'Bolsa com alça superior Carta Gráfica Bloco de cores', 1.105, 1, 2, 2);
+INSERT INTO "Produto"  VALUES (DEFAULT, 'EMERY ROSE Mochila Funcional Minimalista Grande capacidade', 1.659, 1, 2, 3);
+INSERT INTO "Produto"  VALUES (DEFAULT, 'DORA.C S J Mochila bolsa Feminina Couro', 1.563, 1, 2, 4);
+INSERT INTO "Produto"  VALUES (DEFAULT, 'Saco quadrado de grande capacidade minimalista', 0.959, 1, 2, 4);
+INSERT INTO "Produto"  VALUES (DEFAULT, 'Bolsa Adidas Organizer', 0.989, 1, 1, 5);
+INSERT INTO "Produto"  VALUES (DEFAULT, 'bolsa de lona moda fold', 1.235, 1, 1, 6);
+INSERT INTO "Produto"  VALUES (DEFAULT, 'Bolsa De Ombro Feminina Com Todos Os Jogos Mini Bolsa De Lon', 1.356, 1, 1, 7);
+INSERT INTO "Produto"  VALUES (DEFAULT, 'Boné de beisebol bordado com letras', 0.152, 1, 3, 8);
+INSERT INTO "Produto"  VALUES (DEFAULT, '3 peças boné de beisebol sólido', 0.325, 1, 3, 1);
+INSERT INTO "Produto"  VALUES (DEFAULT, 'Capacete Moto R8 Pro Tork Fechado 60 Preto/Prata Viseira Fumê', 1.800, 1, 4, 2);
+INSERT INTO "Produto"  VALUES (DEFAULT, 'Capacete Fechado Stealth Solid 58 Viseira Fumê Preto Metalico', 1.880, 1, 4, 3);
+INSERT INTO "Produto"  VALUES (DEFAULT, 'Chapéu Americano Country Em Couro Legitimo Cowboy', 0.546, 1, 5, 4);
+INSERT INTO "Produto"  VALUES (DEFAULT, 'Chapéu Couro Cabana Nordestino Vaqueiro Cowboy', 0.465, 1, 5, 5);
+INSERT INTO "Produto"  VALUES (DEFAULT, 'Chapéu de dois tons', 0.326, 1, 6, 6);
+INSERT INTO "Produto"  VALUES (DEFAULT, '4 peças de cinto de fivela geométrica', 0.411, 1, 7, 7);
+INSERT INTO "Produto"  VALUES (DEFAULT, 'Cinto Masculino William Polo Original Em Couro Legítimo Modelo Elegant', 0.201, 1, 7, 8);
+INSERT INTO "Produto"  VALUES (DEFAULT, 'GRAVATA BUSINESS PREMIUM REGULAR', 0.222, 1, 8, 1);
+INSERT INTO "Produto"  VALUES (DEFAULT, 'Gravata Tradicional Luxo Homens Slim Fit.', 0.159, 1, 8, 3);
+INSERT INTO "Produto"  VALUES (DEFAULT, 'Sombrinha Guarda-Chuva Dobrável Semi Automático Masculina Negócios Preto', 0.475, 1, 9, 2);
+INSERT INTO "Produto"  VALUES (DEFAULT, 'guarda-chuva dobrável automático com luz led à prova de vento portátil', 1.123, 1, 9, 5);
 
 ----> REALIZANDO CONSULTAS
 SELECT * FROM "Cidade"
